f07977f51b3d10d8946a6d6cbf0046334a564c69
Himanshu Singh <himanshu16.singh@tataaig.com>
2025-07-21 13:50:23 +0530

    feat: updating server port

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  build: {
-    outDir: "dist/spa",
-  },
+export default defineConfig(() => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
